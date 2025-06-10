d366b92df93fb9e9df84560c482f61517e417ef5
Jorgebs12 <jorge.bs1297@gmail.com>
2025-06-10 12:31:37 +0200

    v5

--- islands/Fav.tsx
+++ islands/Fav.tsx
@@ -10,9 +10,12 @@ const Fav: FunctionalComponent<Props> = ({id}) => {
     const [isFav, setIsFav] = useState(false)
 
     useEffect(() => {
+
+        //Buscamos la cookie
         const cookie = document.cookie.split("; ").find((e) => e.startsWith("favCharacter="))
 
         if(cookie) {
+            //Se decodifica la cookie y se comprueba si el id está en la lista de favoritos. Si está, pone isFav a true.
             const favs = JSON.parse(decodeURIComponent(cookie.split("=")[1]))
             setIsFav(favs.includes(id))
         }
@@ -20,14 +23,19 @@ const Fav: FunctionalComponent<Props> = ({id}) => {
     }, [id])
 
     const toogleFav = () => {
+        //Buscamos la cookie
         const cookie = document.cookie.split("; ").find((e) => e.startsWith("favCharacter="))
 
+        //Creamos array vacio para guardar los favoritos
         let favs: string [] = []
 
+        //Si existe la cookie, se decodifica y se convierte de JSON a array de strings.
         if(cookie) favs = JSON.parse(decodeURIComponent(cookie.split("=")[1]))
         
+        //Si el personaje ya es favorito (isFav es true), lo elimina del array de favoritos. Si no lo es, lo añade al array.
         const updateFav = isFav ? favs.filter((f) => f !== id) : [...favs, id]
 
+        //Actualizamos la cookie favCharacter con la nueva lista de favoritos
         document.cookie = `favCharacter=${encodeURIComponent(JSON.stringify(updateFav))}; Path=/`;
 
         setIsFav(!isFav);
--- routes/(platform)/fav-list.tsx
+++ routes/(platform)/fav-list.tsx
@@ -13,17 +13,22 @@ type Character = {
 export const handler: Handlers<{ character: Character}> = {
     async GET(req: Request, ctx: FreshContext) {
 
+        //Obtenemos la cabecera Cookie
         const cookie = req.headers.get("Cookie")
+        //Buscamos la cookie favCharacter en las cookies.
         const favCookie = cookie?.split("; ").find((c) => c.startsWith("favCharacter="))
 
+        //Creamos un array vacío para los favoritos.
         let favNames: string [] = []
 
+        //Si existe la cookie, se decodifica y se convierte de JSON a array de strings.
         if(favCookie) favNames = JSON.parse(decodeURIComponent(favCookie.split("=")[1]))
 
         const response = await axios.get("https://hp-api.onrender.com/api/characters")
 
         const character: Character[] = response.data
 
+        //Filtramos los personajes para quedarnos solo con los que están en la lista de favoritos.
         const filter = character.filter((char) => favNames.includes(char.name))
 
         return ctx.render ({character: filter})
